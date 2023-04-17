ff5020922d81dc583aefbca31f5140a343aea697
GUOGG <2311619185@qq.com>
2023-04-17 22:22:54 +0800

    归并排序

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include"MonotonicStack.h"
 #include"Sort.h"
 int main() {
-	std::vector<int> v{ 6,1,2,7,3,4,16,11,5,1,2,3,7,5,66,77,22,99,44,88,33,14 };
-	Sort::shellSort(v);
+	std::vector<int> v{14,52,4,6,2,7,8,13,41, 6,1,2,7,3,4,16,11,5,1,2,3,7,5,66,77,22,99,44,88,33,14 };
+	Sort::mergeSort(v,0,v.size()-1);
 	for (int i : v) {
 		std::cout << i << std::endl;
 	}
